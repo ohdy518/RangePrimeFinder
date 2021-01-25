e283144b08f226c9a409b769e4cb252ed7667c25
codingCup04 <68053011+codingCup04@users.noreply.github.com>
2021-01-25 17:17:24 +0900

    Added search program

--- findprime.py
+++ findprime.py
@@ -31,25 +31,29 @@ print(f"Not prime count: {len(notprime)}")
 minnum = int(rng[0])
 searching = True
 while searching:
+
     search = input("Search (or .quit to quit): ")
     try:
-        int(search)
-    except:
-        if search == ".quit":
-            searching = False
-            break
-        else:
-            print("Invalid search key.")
-    else:
         try:
-            intsea = int(search)
-            index = prime.index(intsea)
-            print(f"{search} is {str(index)}(th) prime.")
+            int(search)
         except:
-            intsea = int(search)
-            notprime.index(intsea)
-            print(f"{search} is not a prime.")
-            for i in prime:
-                if intsea % i == 0:
-                    print(f"{intsea} divides into {i}. ({intsea} / {i} = {intsea / i})")
+            if search == ".quit":
+                searching = False
+                break
+            else:
+                print("Invalid search key.")
+        else:
+            try:
+                intsea = int(search)
+                index = prime.index(intsea)
+                print(f"{search} is {str(index)}(th) prime.")
+            except:
+                intsea = int(search)
+                notprime.index(intsea)
+                print(f"{search} is not a prime.")
+                for i in prime:
+                    if intsea % i == 0:
+                        print(f"{intsea} divides into {i}. ({intsea} / {i} = {intsea / i})")
+    except:
+        print("Error: Number out of range")
                     
